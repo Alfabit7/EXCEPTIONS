c7515621e20f1822048f4111355a8f31100c1b6c
andrey <tritg@yandex.ru>
2023-06-27 22:26:35 +0300

    added solution task_2

---
+++ homework/hw_2/task_3.java
@@ -0,0 +1,11 @@
+/*
+ * Дан следующий код, исправьте его там, где требуется (задание 3
+ * https://docs.google.com/document/d/
+ * 17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+ */
+
+package hw_2;
+
+public class task_3 {
+
+}
